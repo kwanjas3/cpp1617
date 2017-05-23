2d20f6737b6b601514dc707e55ba777dc15bd522
kwanjas3 <kwanjas3@users.noreply.github.com>
2017-05-23 14:28:36 -0400

    in lab done

---
+++ WS02/in-lab/Kingdom.cpp
@@ -0,0 +1,30 @@
+/***********************************************************
+// OOP244 Workshop 2: Dynamic Memory
+// File Kingdom.cpp
+// Version 1.0
+// Date ???????????
+// Author ?????????
+// Description
+// ?????????????????????
+//
+//
+// Revision History
+///////////////////////////////////////////////////////////
+// Name     Date    Reason
+// 
+///////////////////////////////////////////////////////////
+***********************************************************/
+
+
+
+// TODO: include the necessary headers
+#include <iostream>
+#include "Kingdom.h"
+using namespace std;
+// TODO: the sict namespace
+namespace sict {
+    // TODO:definition for display(...) 
+	void display( const Kingdom& kingdom ) {
+		cout << kingdom.m_name << ", " << kingdom.m_population << endl;
+	}
+}
