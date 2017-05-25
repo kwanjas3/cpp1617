1bcbf8f5bc18dbfd386743a675f51060d3126f20
kwanjas3 <kwanjas3@users.noreply.github.com>
2017-05-25 15:04:53 -0400

    changes to format and text

---
+++ WS02/at-home/kingdom.cpp
@@ -0,0 +1,46 @@
+/***********************************************************
+// OOP244 Workshop 2: Dynamic Memory
+// File Kingdom.cpp
+// Version 1.0
+// Date ???????????
+// Author ?????????
+// Description
+// ?????????????????????
+//
+//
+// Revision History
+///////////////////////////////////////////////////////////
+// Name     Date    Reason
+//
+///////////////////////////////////////////////////////////
+***********************************************************/
+
+
+
+// TODO: include the necessary headers
+#include <iostream>
+#include "Kingdom.h"
+using namespace std;
+// TODO: the sict namespace
+namespace sict {
+	// TODO:definition for display(...) 
+	void display(const Kingdom& kingdom) {
+		cout << kingdom.m_name << ", " << "population " << kingdom.m_population << endl;
+	}
+	void display(const Kingdom * kingdom, int sizeofKingdom)
+	{
+		int i;
+		int sum = 0;
+		for (int i = 0; i < sizeofKingdom; i++) {
+			sum += kingdom[i].m_population;
+		}
+		
+		cout << "Kingdoms are" << endl;
+		for (i = 0; i < sizeofKingdom; i++) {
+			cout << i + 1 << ". " << kingdom[i].m_name << ", " << kingdom[i].m_population << endl;
+		}
+		cout << "------------------------------" << endl;
+		cout << "Total population of all Kingdoms: " << sum << endl;
+		cout << "------------------------------" << endl;
+	}
+}
--- WS02/at-home/w2_at_home.cpp
+++ WS02/at-home/w2_at_home.cpp
@@ -11,7 +11,7 @@
 // Revision History
 ///////////////////////////////////////////////////////////
 // Name     Date    Reason
-// 
+//
 ///////////////////////////////////////////////////////////
 ***********************************************************/
 
@@ -25,78 +25,77 @@ using namespace sict;
 void read(Kingdom&);
 
 int main() {
-    int count = 0; // the number of kingdoms in the array
+	int count = 0; // the number of kingdoms in the array
 
-    // TODO: declare the pKingdom pointer here (don't forget to initialize it)
-    Kingdom* pKingdom = nullptr;
-    cout << "==========\n"
-        << "Input data\n"
-        << "==========\n"
-        << "Enter the number of kingdoms: ";
-    cin >> count;
-    cin.ignore();
+	// TODO: declare the pKingdom pointer here (don't forget to initialize it)
+	Kingdom* pKingdom = nullptr;
+	cout << "==========\n"
+		<< "Input data\n"
+		<< "==========\n"
+		<< "Enter the number of kingdoms: ";
+	cin >> count;
+	cin.ignore();
 
-    if (count < 1) return 1;
+	if (count < 1) return 1;
 
-    // TODO: allocate dynamic memory here for the pKingdom pointer
-	 pKingdom = new Kingdom[count];
-    for (int i = 0; i < count; ++i) {
-        cout << "Kingdom #" << i + 1 << ": " << endl;
-        // TODO: add code to accept user input for Kingdom i
+	// TODO: allocate dynamic memory here for the pKingdom pointer
+	pKingdom = new Kingdom[count];
+	for (int i = 0; i < count; ++i) {
+		cout << "Kingdom #" << i + 1 << ": " << endl;
+		// TODO: add code to accept user input for Kingdom i
 		read(pKingdom[i]);
-    }
-    cout << "==========" << endl << endl;
+	}
+	cout << "==========" << endl << endl;
 
-    // testing that "display(...)" works
-    cout << "------------------------------" << endl
-        << "The first kingdom entered is" << endl
-        << "------------------------------" << endl;
-    display(pKingdom[0]);
-    cout << "------------------------------" << endl << endl;
+	// testing that "display(...)" works
+	cout << "------------------------------" << endl
+		<< "The first kingdom entered is" << endl
+		<< "------------------------------" << endl;
+	display(pKingdom[0]);
+	cout << "------------------------------" << endl << endl;
 
-    // expand the array of Kingdoms by 1 element
-    // TODO: allocate dynamic memory for count + 1 Kingdoms
-	Kingdom* eKingdom = new Kingdom[count+1];
-    // TODO: copy elements from original array into this newly allocated array
+	// expand the array of Kingdoms by 1 element
+	// TODO: allocate dynamic memory for count + 1 Kingdoms
+	Kingdom* eKingdom = new Kingdom[count + 1];
+	// TODO: copy elements from original array into this newly allocated array
 	for (int i = 0; i < count; i++) {
 		strcpy(eKingdom[i].m_name, pKingdom[i].m_name);
 		eKingdom[i].m_population = pKingdom[i].m_population;
 	}
-    // TODO: deallocate the dynamic memory for the original array
+	// TODO: deallocate the dynamic memory for the original array
 	delete[] pKingdom;
-    // TODO: copy the address of the newly allocated array into pKingdom pointer
+	// TODO: copy the address of the newly allocated array into pKingdom pointer
 	pKingdom = eKingdom;
-    // add the new Kingdom
-    cout << "==========\n"
-        << "Input data\n"
-        << "==========\n"
-        << "Kingdom #" << count + 1 << ": " << endl;
-      // TODO: accept input for the new element in the array
-		// TODO: add code to accept user input for Kingdom i 
-		read(pKingdom[count]);
-	
-    count++;
-    cout << "==========\n" << endl;
+	// add the new Kingdom
+	cout << "==========\n"
+		<< "Input data\n"
+		<< "==========\n"
+		<< "Kingdom #" << count + 1 << ": " << endl;
+	// TODO: accept input for the new element in the array
+	read(pKingdom[count]);
 
-    // testing that the overload of "display(...)" works
-    display(pKingdom, count);
-    cout << endl;
+	count++;
+	cout << "==========\n" << endl;
 
-    // TODO: deallocate the dynamic memory here
+	// testing that the overload of "display(...)" works
+	display(pKingdom, count);
+	cout << endl;
+
+	// TODO: deallocate the dynamic memory here
 	delete[] eKingdom;
 	eKingdom = nullptr;
-   pKingdom = nullptr;
-    return 0;
+	pKingdom = nullptr;
+	return 0;
 }
 
 // read accepts data for a Kingdom from standard input
 //
 void read(Kingdom& kingdom) {
 
-    cout << "Enter the name of the kingdom: ";
-    cin.get(kingdom.m_name, 32, '\n');
-    cin.ignore(2000, '\n');
-    cout << "Enter the number people living in " << kingdom.m_name << ": ";
-    cin >> kingdom.m_population;
-    cin.ignore(2000, '\n');
+	cout << "Enter the name of the kingdom: ";
+	cin.get(kingdom.m_name, 32, '\n');
+	cin.ignore(2000, '\n');
+	cout << "Enter the number people living in " << kingdom.m_name << ": ";
+	cin >> kingdom.m_population;
+	cin.ignore(2000, '\n');
 }
