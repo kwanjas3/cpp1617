4020a3fafc72bb3a6e02260ff9394414349add2b
kwanjas3 <kwanjas3@users.noreply.github.com>
2017-07-04 14:06:25 -0400

    in-lab done

---
+++ WS07/in-lab/Hero.h
@@ -0,0 +1,47 @@
+#ifndef HERO_H
+#define HERO_H
+
+#include <iostream>
+
+class Hero {
+    char m_name[41];    
+    int  m_attack;
+    int  m_maximumHealth;
+    int  m_health; 
+	/////////////////////////////////////////////////
+	   // Hero::isEmpty ()
+	   // return true if the Hero object is uninitialized
+	   //
+	bool isEmpty() const
+	{
+		bool check = false;
+		if (m_name[0] == 0 &&
+			m_attack == 0 &&
+			m_maximumHealth == 0 &&
+			m_health == 0
+			) {
+			check = true;
+		}
+		return check;
+	}
+
+public:
+    // constructors
+    Hero ();
+	void setEmpty();
+    Hero (const char name[], int maximumHealth, int attack);
+
+    // member functions
+    void respawn();
+	bool isAlive() const; 
+	int  getAttack() const;
+    void deductHealth(int);
+	void display(std::ostream& os);
+    // friend helper function to insert name into ostream
+	friend std::ostream& operator<<(std::ostream&, const Hero&);
+};
+
+void applyDamage (Hero& A, Hero& B);
+const Hero & operator*(const Hero &, const Hero &);
+
+#endif
